fe6557958b3e2c725c153a541cf71f5e27c726c9
ILCart <143832706+ILCart@users.noreply.github.com>
2024-03-14 22:46:47 -0700

    brand new!

--- script.js
+++ script.js
@@ -48,7 +48,6 @@ utils.getJSON = async function (url) {
 }
 
 async function init() {
-    updateTable();
     //get the root element of the web page
     let root = document.querySelector('#root');
     if (!root) return
@@ -133,7 +132,7 @@ function buildChart(id, data) {
     return chart
 }
 
-
+const usNumberFormat = new Intl.NumberFormat('en-US', { minimumIntegerDigits: 1, minimumFractionDigits: 2 });
 function buildTableRow(tds, sign = "$") {
     let trs = []
     for (const td of tds) {
@@ -144,7 +143,7 @@ function buildTableRow(tds, sign = "$") {
         newth.innerText = td[0].replaceAll("-", " ");
         dollar.innerText = sign
 
-        newtd.innerText = new Intl.NumberFormat('en-US', { minimumIntegerDigits: 1, minimumFractionDigits: 2 }).format(td[1]);
+        newtd.innerText = usNumberFormat.format(td[1]);
         newtr.appendChild(newth);
         newtr.appendChild(dollar);
         newtr.appendChild(newtd);
@@ -153,31 +152,48 @@ function buildTableRow(tds, sign = "$") {
     return trs;
 }
 
+function updateBilboardChart(chart, data) {
+    if (!chart || !data) return;
+
+    chart.load({
+        columns: data.map(slice => [(slice[0][0].toUpperCase() + slice[0].slice(1)).replaceAll("-", " "), Math.abs(slice[1])]),
+        append: false
+    });
+    chart.flush(true);
+}
+
 function updateTable(chart) {
 
     let data = getFormData("form1", "form2");
     if (data.length == 0) return;
     let dataArray0 = Object.entries(data[0]);
     let dataArray1 = Object.entries(data[1]);
-    if (chart) {
-        let chartData = {
-            padding: 30,
-            labels: dataArray1.map(slice => slice[0]),
+    // if (chart) {
+    //     let chartData = {
+    //         padding: 30,
+    //         labels: dataArray1.map(slice => slice[0]),
 
-            datasets: [{
-                data: dataArray1.map(slice => slice[1]),
-                borderColor: "#FFFFFF",
-                borderWidth: 1,
-                hoverOffset: 30
-            }]
-        };
-        updateChart(chart, chartData);
-    }
+    //         datasets: [{
+    //             data: dataArray1.map(slice => slice[1]),
+    //             borderColor: "#FFFFFF",
+    //             borderWidth: 1,
+    //             hoverOffset: 30
+    //         }]
+    //     };
+    //     updateChart(chart, chartData);
+    // }
+
     let table = document.getElementById("breakdown");
     let income = document.getElementById("incometbody");
     let calc = document.getElementById("calctbody");
     let expenses = document.getElementById("expensestbody");
-
+    let medicare;
+    for (const i in dataArray0) {
+        if (dataArray0[i][0] == "medical-insurance") {
+            medicare = dataArray0.splice(i, 1)[0];
+        }
+    }
+    console.log(medicare, "medi")
     for (const deduction of dataArray0) {
         console.log(deduction);
     }
@@ -185,33 +201,92 @@ function updateTable(chart) {
 
     // cleaneddeduc.splice(0,1)
     let grossyear = dataArray0.slice(0, 1);
+    grossyear[0][1] = grossyear[0][1].replaceAll(/,|\$/g, "");
+
     let grossmonth = Math.round((grossyear[0][1] / 12) * 100) / 100;
     grossyear.push(["gross-monthly-income", grossmonth])
     income.replaceChildren(...buildTableRow(grossyear));
-    let medicare = deductions.pop();
+
     medicare[1] = -(+medicare[1]);
-    deductions = deductions.map((field)=>{
+    deductions = deductions.map((field) => {
         console.log(field)
         field[0] += ` (${field[1]}%)`
-        field[1] = `-${+grossmonth*(field[1]/100)}`
+        field[1] = `-${+grossmonth * (field[1] / 100)}`
         return field
     })
     calc.replaceChildren(...buildTableRow(deductions, "$"));
-    calc.appendChild(...buildTableRow([medicare]));
-    let payrolltotal = deductions.map(e=>e[1]).reduce((a,b)=>{return (+a)+(+b)});
-    payrolltotal+=medicare[1];
+    let payrolltotal = deductions.map(e => e[1]).reduce((a, b) => { return (+a) + (+b) });
+    payrolltotal += medicare[1];
     console.log(payrolltotal);
-    calc.appendChild(...buildTableRow([["total-payroll-deductions",payrolltotal]]));
-    calc.appendChild(...buildTableRow([["Net Monthly Income",grossmonth+payrolltotal]]));
-    const expensesdata = dataArray1.map((expense)=>{
-        return [expense[0],-expense[1]]
+    calc.append(...buildTableRow([medicare, ["total-payroll-deductions", payrolltotal], ["Net Monthly Income", grossmonth + payrolltotal]]));
+    const expensesdata = dataArray1.map((expense) => {
+        return [expense[0], expense[1]]
     })
-   
+
     expenses.replaceChildren(...buildTableRow(expensesdata));
     const bdbody = document.getElementById("bdbody");
-    const expensed = expensesdata.map(e=>e[1]).reduce((a,b)=>{return (+a)+(+b)});
-    bdbody.lastElementChild.lastElementChild.innerText = grossmonth+payrolltotal+expensed;
-    bdbody.firstElementChild.lastElementChild.innerText = ""
+    const expensed = expensesdata.map(e => e[1]).reduce((a, b) => { return (+a) + (+b) });
+    bdbody.children[0].lastElementChild.innerText = usNumberFormat.format(grossmonth);
+    bdbody.children[1].lastElementChild.innerText = usNumberFormat.format(payrolltotal);
+    bdbody.children[2].lastElementChild.innerText = usNumberFormat.format(expensed);
+    let savings = grossmonth + payrolltotal + expensed;
+    let savingselement = bdbody.children[3];
+    savingselement.firstElementChild.innerText = savings > 0 ? "Savings" : "Debt";
+    savingselement.lastElementChild.innerText = usNumberFormat.format(savings);
+    savingselement.style["background-color"] = savings < 0 ? "#842828" : "#288430";
+    if (chart) {
+        chart.load({
+            columns: [["Savings", savings]],
+            append: false
+        });
+        chart.flush();
+        updateBilboardChart(chart, dataArray1);
+    }
+
+}
+function buildFields(name = "New Field", value = "0") {
+    if (!name) return;
+    let formattedname = name.toLowerCase().replaceAll(" ", "-");
+    let newsec = document.createElement("section");
+    newsec.classList.add("flex", "whole", "row", "inputcon")
+    newsec.innerHTML = `
+            <label for="${formattedname}">${name}</label>
+            <input class="text-field whole" value="${value}" type="number" name="${formattedname}" id="${formattedname}">
+        `;
+    return newsec
+}
+
+
+function togglePrompt() {
+    let prompt = document.querySelector("#promptcontainer");
+    if (prompt) {
+        prompt.classList.toggle("hidden");
+    }
+}
+
+function addField(e) {
+    togglePrompt();
+    let sec = document.querySelector("form.whole.flex.row:not(.hidden)").querySelector(".whole .flex .row .formsec:not(.income-container)");
+    console.log(e.target)
+    if (sec) {
+        let name = document.getElementById("newfield").value;
+        name = name.toLowerCase().replaceAll(" ", "-")
+        let value = document.getElementById("newfieldval").value;
+        let iter = 0;
+        while (document.getElementById(`${name}-${iter}`) !== null) {
+            iter++
+        }
+        name = `${name}-${iter}`;
+        sec.appendChild(buildFields(name, value));
+        updateTable();
+    }
+
+}
+
+function plusButtons(btns) {
+    [...btns].forEach(e => {
+        e.addEventListener("click", togglePrompt)
+    });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -219,74 +294,106 @@ document.addEventListener("DOMContentLoaded", function () {
     let data = getFormData("form1", "form2");
     if (data.length == 0) return;
     let dataArray = Object.entries(data[1]);
-    let chartData = {
-        labels: dataArray.map(slice => slice[0][0].toUpperCase()+slice[0].slice(1)),
-
-        datasets: [{
-            data: dataArray.map(slice => slice[1]),
-            borderColor: "#FFFFFF",
-            borderWidth: 1,
-            hoverOffset: 30
-        }]
-    };
-    let tooltip = {
-        yAlign: "bottom",
-        bodyAlign: "center",
-        titleAlign: "center",
-        displayColors: false,
-        bodyColor: "#d2e2e9",
-        titleColor: "#FFFFFF",
-        callbacks: {
-            label: context => {
-                let label = context.dataset.label || '';
-                if (context.parsed !== null) {
-                    label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.parsed);
-                }
-                return label;
-            },
-
-        }
-    }
     let chartSkeleton = {
-        type: 'pie',
-        responsive: true,
-        data: chartData,
-        options: {
+        data: {
+            columns: dataArray.map(slice => [(slice[0][0].toUpperCase() + slice[0].slice(1)).replaceAll("-", " "), Math.abs(slice[1])]),
+            type: "pie",
+            bindto: "#chart"
+        },
 
-            layout: {
-                padding: 10,
-            },
-            plugins: {
-                colors: {
-                    enabled: true,
-                    forceOverride: true
+        pie: {
+            expand: {
+                rate: 1.002,
+                duration: -2,
+            }
+        },
+        tooltip: {
+            format: {
+                title: function (d) {
+                    return 'Data ' + d;
                 },
-                title: {
-                    color: "#FFFFFF",
-                    display: true,
-                    text: 'Monthly Budget Expenses',
-                    font: ctx => {
-                        var size = Math.round(ctx.chart.width / 20);
-                        return {
-                            weight: 'bold',
-                            size: size
-                        };
-                    }
-                }
-                ,
-                tooltip: tooltip,
-                legend: {
-                    display: ctx => {
-                        return ctx.chart.width > 400 ? true : false;
-                    },
-                    labels: {
-                        color: "#FFFFFF"
-                    }
+                value: function (value, ratio, id) {
+                    return `$${usNumberFormat.format(value)}`;
                 }
             }
-        }
+        },
+        transition: {
+            duration: 500
+        },
     }
-    const chart = buildChart("chart", chartSkeleton);
-    document.getElementById("form1").addEventListener("change", () => { updateTable() })
+    let chart = bb.generate(chartSkeleton);
+    // let chartData = {
+    //     labels: dataArray.map(slice => slice[0][0].toUpperCase() + slice[0].slice(1)),
+
+    //     datasets: [{
+    //         data: dataArray.map(slice => slice[1]),
+    //         borderColor: "#FFFFFF",
+    //         borderWidth: 1,
+    //         hoverOffset: 30
+    //     }]
+    // };
+    // let tooltip = {
+    //     yAlign: "bottom",
+    //     bodyAlign: "center",
+    //     titleAlign: "center",
+    //     displayColors: false,
+    //     bodyColor: "#d2e2e9",
+    //     titleColor: "#FFFFFF",
+    //     callbacks: {
+    //         label: context => {
+    //             let label = context.dataset.label || '';
+    //             if (context.parsed !== null) {
+    //                 label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.parsed);
+    //             }
+    //             return label;
+    //         },
+
+    //     }
+    // }
+    // let chartSkeleton = {
+    //     type: 'pie',
+    //     responsive: true,
+    //     data: chartData,
+    //     options: {
+
+    //         layout: {
+    //             padding: 10,
+    //         },
+    //         plugins: {
+    //             colors: {
+    //                 enabled: true,
+    //                 forceOverride: true
+    //             },
+    //             title: {
+    //                 color: "#FFFFFF",
+    //                 display: true,
+    //                 text: 'Monthly Budget Expenses',
+    //                 font: ctx => {
+    //                     var size = Math.round(ctx.chart.width / 20);
+    //                     return {
+    //                         weight: 'bold',
+    //                         size: size
+    //                     };
+    //                 }
+    //             }
+    //             ,
+    //             tooltip: tooltip,
+    //             legend: {
+    //                 display: ctx => {
+    //                     return ctx.chart.width > 400 ? true : false;
+    //                 },
+    //                 labels: {
+    //                     color: "#FFFFFF"
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    updateTable(chart);
+    // const chart = buildChart("chart", chartSkeleton);
+    document.getElementById("form1").addEventListener("change", () => { updateTable(chart) })
     document.getElementById("form2").addEventListener("change", () => { updateTable(chart) })
+    plusButtons(document.getElementsByClassName("plus"));
+    document.getElementById("promptsubmit").addEventListener("click", (e) => { addField(e); updateTable(chart); });
+    document.getElementById("exitprompt").addEventListener("click", togglePrompt);
 });
